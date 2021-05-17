428793b410ed6b09603cda68d747ab7385c243dd
Igor Davoli <igordavoli@gmail.com>
2021-05-17 17:51:19 -0300

    added dialectOptions.rejectUnauthorized: false

--- src/config/database/sequelize.js
+++ src/config/database/sequelize.js
@@ -10,4 +10,10 @@ module.exports = {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  }
 };
