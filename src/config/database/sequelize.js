df030a5c1e1f527aa6ed940a22ac421152d1dd1f
Igor Davoli <igordavoli@gmail.com>
2021-05-19 13:25:19 -0300

    Response return all missong fields auth.controller

--- src/config/database/sequelize.js
+++ src/config/database/sequelize.js
@@ -2,18 +2,6 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config({ path: path.join(__dirname, '../../../.env') });
-// const db = {
-//   test: 'db_test',
-//   production: ,
-//   development: 'postgres',
-// }
-
-// const options = {
-//   production: { ssl: { require: true, rejectUnauthorized: false } },
-//   test: {},
-//   development: {},
-// }
-
 
 module.exports = {
   username: process.env.DB_USER,
