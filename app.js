88070ae4b4140f3ebefc60d1e0226a3ebac5e3fb
Igor Davoli <igordavoli@gmail.com>
2021-05-21 23:08:02 -0300

    Added env config.

--- app.js
+++ app.js
@@ -6,3 +6,5 @@ app.listen(port, () => console.info(
   `Application currently running on port: ${port} for ${process.env.NODE_ENV}`,
 ),
 );
+
+module.exports = app;
--- src/config/database/sequelize.js
+++ src/config/database/sequelize.js
@@ -3,17 +3,24 @@ const path = require('path');
 
 dotenv.config({ path: path.join(__dirname, '../../../.env') });
 
+const databaseName = {
+  test: process.env.DB_TEST_NAME,
+  development: process.env.DB_DEV_NAME,
+  production: process.env.DB_NAME,
+}
+
+
 module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  database: databaseName[process.env.NODE_ENV],
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   dialect: 'postgres',
   dialectOptions: {
-    // ssl: {
-    //   require: true,
-    //   rejectUnauthorized: false,
-    // }
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
   }
 };
