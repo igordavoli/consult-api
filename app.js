b64b28ef8a66afefc1df578bd5d654e07b79dea9
Igor Davoli <igordavoli@gmail.com>
2021-05-24 18:26:58 -0300

    Fixed test with multiple files.

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const app = require('./src/config/express');
 
 const port = app.get('port');
 
-app.listen(port, () => console.info(
-  `Application currently running on port: ${port} for ${process.env.NODE_ENV}`,
-),
-);
+if (process.env.NODE_ENV !== 'test') {
+
+  app.listen(port, () => console.info(
+    `Application currently running on port: ${port} for ${process.env.NODE_ENV}`,
+  )
+  );
+}
 
 module.exports = app;
