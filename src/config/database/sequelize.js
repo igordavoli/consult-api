cd525b3417139b848c04ccd4eb4acd6e3f600fc8
Igor Davoli <igordavoli@gmail.com>
2021-05-24 13:51:40 -0300

    First six test for users.

--- src/config/database/sequelize.js
+++ src/config/database/sequelize.js
@@ -2,21 +2,46 @@ const dotenv = require('dotenv');
 const path = require('path');
 
 dotenv.config({ path: path.join(__dirname, '../../../.env') });
+dotenv.config({ path: path.join(__dirname, '../../../.env.dev') });
+dotenv.config({ path: path.join(__dirname, '../../../.env.test') });
 
-module.exports = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  define: {
-    underscored: true
-  },
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+const databaseConfig = {
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    define: { underscored: true },
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
+  },
+  development: {
+    username: process.env.DEV_DB_USER,
+    password: process.env.DEV_DB_PASS,
+    database: process.env.DEV_DB_NAME,
+    host: process.env.DEV_DB_HOST,
+    port: process.env.DEV_DB_PORT,
+    define: { underscored: true },
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.TEST_DB_USER,
+    password: process.env.TEST_DB_PASS,
+    database: process.env.TEST_DB_NAME,
+    host: process.env.TEST_DB_HOST,
+    port: process.env.TEST_DB_PORT,
+    define: { underscored: true },
+    dialect: 'postgres',
   }
-};
+}
+
+
+console.log(databaseConfig[process.env.NODE_ENV])
+
+module.exports = databaseConfig[process.env.NODE_ENV];
---
+++ src/tests/User.test.js
@@ -0,0 +1,97 @@
+const request = require('supertest');
+const app = require('../../app');
+const sequelize = require('sequelize')
+// const getConnection = require('sequelize');
+// const createConnection = require('../config/database/');
+
+describe('User', () => {
+
+  it('Should BE able to create a new user',
+    async () => {
+      const res = await request(app).post('/api/v1/auth/signup')
+        .send({
+          email: 'user@exemple.com',
+          firstName: 'user',
+          lastName: 'doo',
+          telephone: '11123456789',
+          password: 'user123Exemple',
+        });
+
+      expect(res.status).toBe(201);
+      expect(res.body).toHaveProperty('token');
+      expect(res.body).toHaveProperty('storedUser');
+    });
+
+  it('Should NOT be able to create a new user when already exists a user with the same EMAIL.',
+    async () => {
+      const res = await request(app).post('/api/v1/auth/signup')
+        .send({
+          email: 'user@exemple.com',
+          firstName: 'user',
+          lastName: 'doo',
+          telephone: '11123456789',
+          password: 'user123Exemple',
+        });
+
+      expect(res.status).toBe(409);
+      expect(res.body).toMatch('email-already-registered');
+    });
+
+  it('Should BE able to create a new user when already exists a user with the same first and last name.',
+    async () => {
+      const res = await request(app).post('/api/v1/auth/signup')
+        .send({
+          email: 'user2@exemple.com',
+          firstName: 'user',
+          lastName: 'doo',
+          telephone: '11123456789',
+          password: 'user123Exemple',
+        });
+
+      expect(res.status).toBe(201);
+      expect(res.body).toHaveProperty('token');
+      expect(res.body).toHaveProperty('storedUser');
+    });
+
+
+  // /*
+  //  *|> SINGIN IN
+  //  */
+
+  it('Should BE able to login a user when inputted a existent email and a corespondent password.',
+    async () => {
+      const res = await request(app).post('/api/v1/auth/signin')
+        .send({
+          email: 'user@exemple.com',
+          password: 'user123Exemple',
+        });
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('token');
+      expect(res.body).toHaveProperty('user');
+    });
+
+  it('Should be able to NOT permit a login when inputted a inexistent email.',
+    async () => {
+      const res = await request(app).post('/api/v1/auth/signin')
+        .send({
+          email: 'user@fail-exemple.com',
+          password: 'user123Exemple',
+        });
+
+      expect(res.status).toBe(404);
+      expect(res.body).toMatch('user-not-found');
+    });
+
+  it('Should be able to NOT permit a login when inputted a incorrect password.',
+    async () => {
+      const res = await request(app).post('/api/v1/auth/signin')
+        .send({
+          email: 'user@exemple.com',
+          password: 'userExemple123',
+        });
+
+      expect(res.status).toBe(401);
+      expect(res.body).toMatch('invalid-password');
+    });
+});
