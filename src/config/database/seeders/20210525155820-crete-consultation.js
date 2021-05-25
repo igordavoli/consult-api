ce5020a22333d051833098a3bee5f68dbcc9edb5
Igor Davoli <igordavoli@gmail.com>
2021-05-25 14:27:44 -0300

    fixed consultation seeder file.

--- src/config/database/seeders/20210525155820-crete-consultation.js
+++ src/config/database/seeders/20210525155820-crete-consultation.js
@@ -5,15 +5,16 @@ module.exports = {
     .bulkInsert('consultations', [
       {
         id: 'aade9ce5-a525-46c8-bb59-983f7e7672d9',
-        userId: '4f006377-a49c-4434-bbde-ad7848ad43dd',
+        user_id: '4f006377-a49c-4434-bbde-ad7848ad43dd',
         status: 'created',
-        professionalId: '82409d97-6e54-4881-a5e5-5021f97ac21e',
+        professional_id: '82409d97-6e54-4881-a5e5-5021f97ac21e',
         reason: 'Problems',
-        updatedAt: '2021-05-25T02:48:18.317Z',
-        createdAt: '2021-05-25T02:48:18.317Z'
+        updated_at: '2021-05-25T02:48:18.317Z',
+        created_at: '2021-05-25T02:48:18.317Z'
       }
     ]),
 
   down: async (queryInterface, Sequelize) => queryInterface
     .bulkDelete('consultations', null, {}),
 };
+
