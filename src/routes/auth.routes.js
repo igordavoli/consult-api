c2d5487730826bd45bb8563ed38e2d512d0b6dd9
Igor Davoli <igordavoli@gmail.com>
2021-06-01 21:47:21 -0300

    sanving images in uploads folder.

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,17 @@
 // eslint-disable-next-line new-cap
 const router = require('express').Router();
 const { authController } = require('../controllers');
+const multer = require('multer');
+const uploadStorage = require('../config/upload');
+
+const upload = multer({ storage: uploadStorage });
 
 router.post('/signin', authController.signin);
+
 router.post('/signup', authController.signup);
+
 router.post('/signinpro', authController.signinPro);
-router.post('/signuppro', authController.signupPro);
+
+router.post('/signuppro', upload.single('photo'), authController.signupPro);
 
 module.exports.auth = router;
